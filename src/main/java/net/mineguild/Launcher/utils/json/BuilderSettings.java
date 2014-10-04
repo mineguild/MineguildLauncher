875503461685b5991f7646896f107caa706a3559
nylser <devnull@localhost>
2014-10-04 17:05:55 +0200

    Forgot to add new files.

---
+++ src/main/java/net/mineguild/Launcher/utils/json/BuilderSettings.java
@@ -0,0 +1,19 @@
+package net.mineguild.Launcher.utils.json;
+
+import java.io.File;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import com.google.gson.annotations.Expose;
+
+public class BuilderSettings {
+  
+  private @Expose @Getter @Setter String lastPath;
+  
+  public BuilderSettings(){
+    lastPath = new File("").getAbsolutePath();
+  }
+  
+
+}
---
+++ src/main/java/net/mineguild/Launcher/utils/json/MCVersionIndex.java
@@ -0,0 +1,19 @@
+package net.mineguild.Launcher.utils.json;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class MCVersionIndex {
+  
+  public Map<String, String> latest;
+  public List<MCVersion> versions;
+  
+  public class MCVersion {
+    public String id;
+    public Date time;
+    public Date releaseTime;
+    public String type;
+  }
+
+}
